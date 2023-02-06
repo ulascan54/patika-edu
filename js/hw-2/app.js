002e9b55b0d2382a7f983fdf8fe44c703bb2826a
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-02-06 13:15:32 +0300

    complate homework-2

--- js/hw-2/app.js
+++ js/hw-2/app.js
@@ -1,22 +1,94 @@
-var userName = prompt("Adınız Nedir ?" ,"Adınızı Giriniz")
-document.getElementById('myName').innerHTML=userName
-
-var myClock = document.getElementById('myClock')
-function currentTime() {
-    var cd = new Date();
-    var s = cd.getSeconds();
-    var m = cd.getMinutes();
-    var h = cd.getHours();
-    var d = cd.getDay()
-    var currentDay = ""
-    if      (d == 0) { currentDay = "Pazar" }
-    else if (d == 1) { currentDay = "Pazartesi" }
-    else if (d == 2) { currentDay = "Salı" }
-    else if (d == 3) { currentDay = "Çarşamba" }
-    else if (d == 4) { currentDay = "Perşembe" }
-    else if (d == 5) { currentDay = "Cuma" }
-    else{ currentDay = "Cumartesi" }
-    myClock.innerHTML =`${("0"+h).substr(-2)}:${("0"+m).substr(-2)}:${("0"+s).substr(-2)} ${currentDay}`
+let taskList = []
+if(localStorage.getItem("taskList") !== null) {
+    taskList = JSON.parse(localStorage.getItem("taskList"));
 }
-currentTime()
-setInterval(currentTime, 1000);
+
+displayTasks();
+
+function displayTasks() {
+    ul = document.getElementById("list");
+    ul.innerHTML = "";
+    
+
+    for (let task of taskList) {
+
+      let li = `
+      <li onclick="checkListItem(${task.id})" id="${task.id}">
+          ${task.taskName}<button
+          class="btn btn-link"
+          type="button"
+          onclick="deleteTask(${task.id})"
+        >
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+        </li>  
+`;
+        
+
+      ul.insertAdjacentHTML("beforeend", li);
+    }
+  }
+
+
+function checkListItem(id) {
+    let checkedID;
+    for(let item in taskList){
+        if(taskList[item].id == id){
+            checkedID = item;
+        }
+    }
+    let classList = document.getElementById(`${checkedID}`).classList;
+    if (classList.contains("checked")){
+        classList.remove("checked")
+    }else{
+        classList.add("checked");
+    }
+}
+document.querySelector("#task").addEventListener("keypress", function(e) {
+    if ((e.key==="Enter")) {
+        e.preventDefault()
+        newElement()
+    }
+  });
+document.querySelector("#liveToastBtn").addEventListener("onclick", newElement);
+document.querySelector("#liveToastBtn").addEventListener("click", showToast);
+function newElement() {
+    let task = document.querySelector("#task");
+    if(task.value == "") {
+        var toast2DOM = document.querySelector("#liveToast2")
+        var toast2 = new bootstrap.Toast(toast2DOM)
+        toast2.show()
+    }else{
+        taskList.push({
+            id: taskList.length,
+            taskName: task.value
+        })
+        displayTasks();
+        showToast();
+        task.value = "";
+        localStorage.setItem("taskList", JSON.stringify(taskList))
+    }
+}
+
+function showToast() {
+    let task = document.querySelector("#task")
+    if(!task.value == ""){
+        var toastDOM = document.querySelector("#liveToast")
+        var toast = new bootstrap.Toast(toastDOM)
+        toast.show()
+    }
+}
+
+
+function deleteTask(id) {
+    let deletedID;
+    for (let index in taskList) {
+        if(taskList[index].id == id) {
+            deletedID = index;
+        }
+    }
+    
+    taskList.splice(deletedID, 1);
+    displayTasks();
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+}
