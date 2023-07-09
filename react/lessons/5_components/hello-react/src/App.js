6b9648f2ed07e68010b5ebf0892266e7dfcd3cfd
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-09 13:53:14 +0300

    components and variables

--- react/lessons/5_components/hello-react/src/App.js
+++ react/lessons/5_components/hello-react/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 // import React from 'react';
 import Header from './components/Header'
+
+const name ='Ulas'
+const surName='can'
 function App(){
   // return React.createElement('div',null,'hello')
   return(
@@ -8,7 +11,8 @@ function App(){
       <h1 className='xyz'>Hello React</h1>
       <Header/>
       <label htmlFor='myinput'>
-        name
+        {/* {name}{surName} */}
+        {`my name is ${name} surname is ${surName}`}
         <input id='myinput' />
       </label>
     </>
