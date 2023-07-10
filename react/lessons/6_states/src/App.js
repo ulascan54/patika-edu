48cf2b6cb61479be42be4c0f94afe777ed6ac970
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-10 17:30:50 +0300

    array states

--- react/lessons/6_states/src/App.js
+++ react/lessons/6_states/src/App.js
@@ -3,13 +3,23 @@ import { useState } from "react"
 function App() {
   const [name, setName] = useState("Ulas")
   const [age, setAge] = useState(18)
-
+  const [friends, setFriends] = useState(["ulas", "can"])
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h2>{age}</h2>
       <button onClick={() => setName("Can")}>Click</button>
       <button onClick={() => setAge(25)}>chenge age</button>
+      <hr />
+      <br />
+      <br />
+      <h2>Friends</h2>
+      {friends.map((friend) => (
+        <div>{friend}</div>
+      ))}
+      <button onClick={() => setFriends([...friends, "ayşe"])}>
+        newfriend
+      </button>
     </div>
   )
 }
