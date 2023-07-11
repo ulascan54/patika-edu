a9e8c2136ded18bc1339952885263a36408070f5
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-11 16:11:24 +0300

    created todos project

---
+++ react/projects/3_hw/src/components/Todos/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+function Todos() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+export default Todos
