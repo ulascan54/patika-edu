ddb1500056ef8d4498949f4c06ae954bbc03f570
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-11 13:03:28 +0300

    complated:form componenet

--- react/projects/2_hw/src/components/Contacts/Form/index.js
+++ react/projects/2_hw/src/components/Contacts/Form/index.js
@@ -1,7 +1,51 @@
-import React from "react"
+import { useState, useEffect } from "react"
 
-function Form() {
-  return <div>Form</div>
+const initialFormValues = { fullname: "", phone_number: "" }
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initialFormValues)
+
+  useEffect(() => {
+    setForm(initialFormValues)
+  }, [contacts])
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (form.fullname === "" || form.phone_number === "") return false
+    addContact([...contacts, form])
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            name="fullname"
+            type="text"
+            placeholder="Full Name"
+            value={form.fullname}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div>
+          <input
+            name="phone_number"
+            type="text"
+            placeholder="Phone Number"
+            value={form.phone_number}
+            onChange={onChangeInput}
+          />
+        </div>
+
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Form
