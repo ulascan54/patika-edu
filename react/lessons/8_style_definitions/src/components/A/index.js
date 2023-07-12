cd88cc7cb27d4b7adf5baee6fb3a74bde8b948b7
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-12 13:55:12 +0300

    module css

---
+++ react/lessons/8_style_definitions/src/components/A/index.js
@@ -0,0 +1,8 @@
+import styles from "./styles.module.css"
+console.log("a", styles)
+
+function A() {
+  return <div className={styles.title}>A</div>
+}
+
+export default A
---
+++ react/lessons/8_style_definitions/src/components/B/index.js
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css"
+console.log("b", styles)
+function B() {
+  return <div className={styles.title}>B</div>
+}
+
+export default B
