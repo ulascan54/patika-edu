2023ac66486f64bcccb97545c9518755a95365ab
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-12 16:46:04 +0300

    native fatching

---
+++ react/lessons/9_fetching/src/components/Users.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+function Users() {
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
+  return (
+    <div>
+      <h1>Users</h1>
+      {isLoading && <div>Loading...</div>}
+
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default Users
