63051a20539d6c1985722a29ece1164aa4bd1dbe
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-15 18:58:33 +0300

    Backend Connection

--- react/lessons/15_chat-app/client/src/components/Container.js
+++ react/lessons/15_chat-app/client/src/components/Container.js
@@ -1,8 +1,13 @@
 import React from "react"
 import ChatForm from "./ChatForm"
 import ChatList from "./ChatList"
+import { init } from "../socketApi"
+import { useEffect } from "react"
 
 function Container() {
+  useEffect(() => {
+    init()
+  }, [])
   return (
     <div className="App">
       <ChatList />
