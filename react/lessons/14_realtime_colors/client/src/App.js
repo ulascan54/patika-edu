b900b7ba17f3d1d0bbd9a5da5c6f390f0a3140b0
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-15 16:36:46 +0300

    subscribe a channel with socket io

--- react/lessons/14_realtime_colors/client/src/App.js
+++ react/lessons/14_realtime_colors/client/src/App.js
@@ -1,15 +1,20 @@
 import "./App.css"
-import { useEffect } from "react"
-import { init } from "./socketApi.js"
+import { useEffect, useState } from "react"
+import { init, subscribe } from "./socketApi.js"
 import Palatte from "./components/Pallatte"
 
 function App() {
+  const [activeColor, setActiveColor] = useState("#000000")
   useEffect(() => {
     init()
+    subscribe((color) => {
+      setActiveColor(color)
+    })
   }, [])
   return (
-    <div className="App">
-      <Palatte />
+    <div className="App" style={{ backgroundColor: activeColor }}>
+      <h1>{activeColor}</h1>
+      <Palatte activeColor={activeColor} />
     </div>
   )
 }
--- react/lessons/14_realtime_colors/client/src/components/Pallatte.js
+++ react/lessons/14_realtime_colors/client/src/components/Pallatte.js
@@ -1,21 +1,20 @@
 import { useState } from "react"
 import { send } from "../socketApi"
-function Palatte() {
+function Palatte({ activeColor }) {
   const [color, setColor] = useState("#000000")
   const handleClick = (e) => {
     e.preventDefault()
     send(color)
-    setColor("#000000")
+    setColor(activeColor)
   }
   return (
     <div className="palatte">
       <input
         type="color"
-        value={color}
+        value={activeColor}
         onChange={(e) => setColor(e.target.value)}
       />
       <button onClick={handleClick}>Click</button>
-      <p>{color}</p>
     </div>
   )
 }
