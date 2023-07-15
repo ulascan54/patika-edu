0b85ca61a9b5504d42fd4614eebf54d20895383e
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-15 15:43:43 +0300

    creted new porject

---
+++ react/lessons/14_realtime_colors/client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import Palatte from "./components/Pallatte"
+
+function App() {
+  return (
+    <div className="App">
+      <Palatte />
+    </div>
+  )
+}
+
+export default App
---
+++ react/lessons/14_realtime_colors/client/src/components/Pallatte.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Palatte() {
+  return (
+    <div className="palatte">
+      <input type="color" />
+      <button>Click</button>
+    </div>
+  )
+}
+
+export default Palatte
