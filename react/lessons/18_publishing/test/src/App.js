756daa18165882ea04a22d48f91e62b772be60b1
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-16 15:39:07 +0300

    Publish Operations

---
+++ react/lessons/18_publishing/test/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+import { Button, ExampleComponent } from "react-publishing-ulas"
+
+import "react-publishing-ulas/dist/index.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="SELAMMM" />
+      <ExampleComponent text="SELAMMM" />
+    </div>
+  )
+}
+
+export default App
