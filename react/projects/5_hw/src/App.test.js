60014072890bf3fb62086c57a5e0393c8d8de6b5
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-16 14:13:17 +0300

    complated testing function for filtered

--- react/projects/5_hw/src/App.test.js
+++ react/projects/5_hw/src/App.test.js
@@ -1,8 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import "@testing-library/jest-dom"
+import App from "./App"
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement("div")
+  ReactDOM.render(<App />, div)
+})
+
+test("texbox should be filtered to emoji list", async () => {
+  render(<App />)
+  const input = screen.getByRole("textbox")
+  const emoji = "100"
+  const item = screen.getByText(/100/i)
+  await userEvent.type(input, emoji)
+  expect(item).toBeInTheDocument()
+})
